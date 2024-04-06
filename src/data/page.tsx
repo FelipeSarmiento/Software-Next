a5d7244425a16214f02e79b35fee7292f179bab9
Felipe Sarmiento | FullStack Developer <f.sarmiento.profesional@gmail.com>
2024-04-06 10:38:40 -0500

    Encrypt password functionality is working now

--- src/data/page.tsx
+++ src/data/page.tsx
@@ -2,13 +2,13 @@
 import {sql} from "@vercel/postgres";
 // import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-
+import bcrypt from "bcrypt";
 // var CryptoJS = require("crypto-js");
 
 export const registerUser = async (users) => {
     const { username, password, email, firstName, lastName, phoneNumber } = users
-    // let passwordEncrypted = bcrypt.hashSync(password, 10);
-    return await sql`INSERT INTO users (first_name, last_name, username, email, phone_number, password) VALUES (${firstName}, ${lastName}, ${username}, ${email}, ${phoneNumber}, ${password}) RETURNING *`;
+    let passwordEncrypted = bcrypt.hashSync(password, 10);
+    return await sql`INSERT INTO users (first_name, last_name, username, email, phone_number, password) VALUES (${firstName}, ${lastName}, ${username}, ${email}, ${phoneNumber}, ${passwordEncrypted}) RETURNING *`;
 }
 // export const login = async (user: any) => {
 //     const { password, email } = user
