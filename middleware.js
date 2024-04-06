2d53a42e58c6efd810dede5881f438df654816e9
Felipe Sarmiento | FullStack Developer <f.sarmiento.profesional@gmail.com>
2024-04-06 17:51:48 -0500

    Session storage functionality is working now, some changes are comming about Session Storage

--- middleware.js
+++ middleware.js
@@ -2,11 +2,11 @@ import { NextResponse } from 'next/server'
 import {getSession} from "@/data/page";
 
 export async function middleware(req) {
-    const session = getSession()
-    if (!session) {
-        return NextResponse.redirect(new URL('/Auth/Login', req.url))
-    }
-    return NextResponse.next()
+    // const session = getSession()
+    // if (!session) {
+    //     return NextResponse.redirect(new URL('/Auth/Login', req.url))
+    // }
+    // return NextResponse.next()
 }
 
 export const config = {
