97d64bb20cf319bd02c5e03d61e941dbb62d850a
Felipe Sarmiento | FullStack Developer <f.sarmiento.profesional@gmail.com>
2024-04-09 00:34:12 -0500

    Some bugs about design on mobile devices are fixed: MyProjectsPage

--- src/app/MyProjects/page.jsx
+++ src/app/MyProjects/page.jsx
@@ -30,45 +30,45 @@ export default function Home() {
     return (
         <>
             <div className="min-h-[calc(100vh_-_84px)] px-4">
-                <header className="h-32 px-12 mb-5 shadow">
+                <header className="h-32 md:px-12 mb-5 shadow">
                     <div className="mx-auto h-full flex items-center justify-between">
-                        <h1 className="flex items-center justify-around text-4xl font-bold tracking-tight text-white">
+                        <h1 className="flex items-center justify-around text-2xl md:text-4xl font-bold tracking-tight text-white">
                             My Projects
                         </h1>
                         <button
-                            className="px-6 border-2 border-white flex items-center space-x-2 font-bold text-white h-12 rounded-md hover:text-cyan-500 hover:border-cyan-500"
+                            className="px-2 md:px-6 border-2 border-white text-sm flex items-center space-x-2 font-bold text-white h-10 md:h-12 rounded-md hover:text-cyan-500 hover:border-cyan-500"
                             onClick={open}>
                             <span>New Project</span>
                             <IconSquareRoundedPlus/>
                         </button>
                     </div>
                 </header>
-                <div className="grid grid-cols-2 w-max mx-auto gap-5">
+                <div className="grid grid-cols-1 md:grid-cols-2 md:w-max mx-auto gap-5">
                 {
                     projects.length > 0 ? projects.map((project, index) => (
-                            <div key={ project.idproject * index } className="w-full grid grid-cols-2 p-6 md:w-[40vw] h-80 rounded-lg border-2 border-neutral-300">
-                                <div className="border-2 border-neutral-300 rounded-md h-full"></div>
-                                <div className="rounded-md h-full px-6 text-white">
-                                    <div className="relative h-1/5 flex items-center text-2xl font-bold py-1">
+                            <div key={ project.idproject * index } className="w-full grid grid-cols-1 md:grid-cols-2 p-2 md:p-6 md:w-[40vw] h-full md:h-80 rounded-lg border-2 border-neutral-300">
+                                <div className="border-2 border-neutral-300 rounded-md h-40 md:h-full"></div>
+                                <div className="rounded-md h-52 flex flex-wrap md:h-full px-3 md:px-6 py-2 text-white">
+                                    <div className="w-full relative h-1/5 flex flex-wrap items-center text-2xl font-bold py-1">
                                         <h3 className="text-cyan-500">{ project.projectname }</h3>
                                         <span className={`absolute top-2/4 -translate-y-2/4 right-0 ${project.ispublic ? 'text-green-500' : 'text-red-500'}`} title={ project.ispublic ? 'Is public' : 'Is not public' }>
                                             <IconWorld/>
                                         </span>
                                     </div>
-                                    <div className="h-2/5 py-1 overflow-hidden text-wrap">
+                                    <div className="w-full h-2/5 py-1 overflow-hidden text-wrap">
                                         <p className="text-ellipsis">{ project.projectdescription }</p>
                                     </div>
-                                    <div className="h-1/5 flex space-x-5 items-center">
-                                        <span className="bg-gray-800 rounded-lg px-1 text-sm">Ecommerce</span>
+                                    <div className="w-2/4 md:w-full h-1/5 flex space-x-5 items-center justify-center md:justify-start">
+                                        <span className="bg-gray-800 rounded-lg px-1 text-xs md:text-sm">Ecommerce</span>
                                     </div>
-                                    <div className="h-1/5 flex space-x-5 items-center justify-center">
+                                    <div className="w-2/4 md:w-full h-1/5 flex space-x-5 items-center justify-center">
                                         <button className="flex space-x-2 hover:text-cyan-500">
                                             <IconExternalLink/>
-                                            <span>Visit</span>
+                                            <span className="hidden md:block">Visit</span>
                                         </button>
                                         <button className="flex space-x-2 hover:text-cyan-500">
                                             <IconEdit/>
-                                            <span>Dashboard</span>
+                                            <span className="hidden md:block">Dashboard</span>
                                         </button>
                                         <button
                                             onClick={() => {
@@ -83,7 +83,7 @@ export default function Home() {
                                             }}
                                             className="flex space-x-2 hover:text-cyan-500">
                                             <IconSettings/>
-                                            <span>Settings</span>
+                                            <span className="hidden md:block">Settings</span>
                                         </button>
                                     </div>
                                 </div>
@@ -163,87 +163,89 @@ const ModalContent = ({ close, project }) => {
     ]
 
     return (
-        <div className="fixed top-0 left-0 w-full flex items-center justify-center h-screen bg-stone-950/90">
-            <div className="relative p-6 w-2/4 bg-black border-2 border-white text-white rounded-lg">
+        <div className="fixed top-0 left-0 w-full pt-20 px-2 flex md:items-center justify-center h-screen  bg-stone-950/90">
+            <div className="relative px-3 py-2 md:py-3 md:px-6 w-full md:w-2/4 bg-black border-2 overflow-y-auto border-white text-white rounded-lg">
                 <h2 className="text-lg font-bold text-center">{ project ? 'Edit Project' : 'Create a new Project' }</h2>
-                <div>
-                    <form onSubmit={ handleProjectForm } className="grid grid-cols-2 gap-x-8">
-                        <div>
-                            <div className="mt-4">
-                                <label htmlFor="project-name" className="block text-sm font-bold py-1">Project Name</label>
-                                <input onChange={ (event) => {
-                                    setProjectForm({ ...projectForm, project_name: event.target.value })
-                                } } type="text" name="project_name" id="project_name" value={ projectForm?.project_name } className="w-full h-10 rounded-md bg-stone-950 border-2 border-white px-2" />
-                            </div>
-                            <div className="mt-4">
-                                <label htmlFor="project-description" className="block text-sm font-bold py-1">Description</label>
-                                <textarea onChange={ (event) => {
-                                    setProjectForm({ ...projectForm, project_description: event.target.value })
-                                }} id="project_description" name="project_description" value={ projectForm?.project_description } rows={3} className="h-20 w-full rounded-md bg-stone-950 border-2 border-white px-2 max-h-32" />
-                            </div>
-                            <div className="mt-4">
-                                <label htmlFor="project-tags" className="block text-sm font-bold py-1">Tags</label>
-                                <input onChange={ (event) => {
-                                    setProjectForm({ ...projectForm, tags: [...projectForm.tags, event.target.value] })
-                                }} type="text" name="tags" id="tags" className="h-10 w-full rounded-md bg-stone-950 border-2 border-white px-2" />
-                            </div>
-                            <div className="mt-4 grid grid-cols-2">
-                                <div className="flex items-center justify-center">
-                                    <Switch
-                                        classNames={{
-                                            track: 'border-2 border-white ' + (projectForm?.isPublic ? 'bg-blue-800' : 'bg-gray-800'),
-                                        }}
-                                        checked={projectForm?.isPublic}
-                                        color="blue"
-                                        onChange={(event) => { setProjectForm({...projectForm, isPublic: event.currentTarget.checked}) }}
-                                        label="Is Public"
-                                    />
+                <div className="">
+                    <form onSubmit={ handleProjectForm }>
+                        <div className="flex flex-wrap md:grid grid-cols-2 gap-x-8 w-full">
+                            <div className="col-span-1 w-full">
+                                <div className="mt-4">
+                                    <label htmlFor="project-name" className="block text-sm font-bold py-1">Project Name</label>
+                                    <input onChange={ (event) => {
+                                        setProjectForm({ ...projectForm, project_name: event.target.value })
+                                    } } type="text" name="project_name" id="project_name" value={ projectForm?.project_name } className="w-full h-10 rounded-md bg-stone-950 border-2 border-white px-2" />
                                 </div>
-                                <div>
-                                    type
+                                <div className="mt-4">
+                                    <label htmlFor="project-description" className="block text-sm font-bold py-1">Description</label>
+                                    <textarea onChange={ (event) => {
+                                        setProjectForm({ ...projectForm, project_description: event.target.value })
+                                    }} id="project_description" name="project_description" value={ projectForm?.project_description } rows={3} className="h-20 w-full rounded-md bg-stone-950 border-2 border-white px-2 max-h-32" />
                                 </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="h-full">
-                                <div className="h-1/6 flex items-end py-1 justify-center border-b-2 font-bold text-lg">
-                                    <span>Template</span>
+                                <div className="mt-4">
+                                    <label htmlFor="project-tags" className="block text-sm font-bold py-1">Tags</label>
+                                    <input onChange={ (event) => {
+                                        setProjectForm({ ...projectForm, tags: [...projectForm.tags, event.target.value] })
+                                    }} type="text" name="tags" id="tags" className="h-10 w-full rounded-md bg-stone-950 border-2 border-white px-2" />
                                 </div>
-                                <div className="h-5/6">
-                                    <div className="h-5/6 flex items-center justify-center">
-                                        { templates[templateSelected].name }
+                                <div className="mt-4 grid grid-cols-2">
+                                    <div className="flex items-center justify-center">
+                                        <Switch
+                                            classNames={{
+                                                track: 'border-2 border-white ' + (projectForm?.isPublic ? 'bg-blue-800' : 'bg-gray-800'),
+                                            }}
+                                            checked={projectForm?.isPublic}
+                                            color="blue"
+                                            onChange={(event) => { setProjectForm({...projectForm, isPublic: event.currentTarget.checked}) }}
+                                            label="Is Public"
+                                        />
                                     </div>
-                                    <div className="relative h-1/6 text-white flex items-center">
-                                        <button type="button"
-                                            onClick={() => {
-                                                if (templateSelected === 0) {
-                                                    setTemplateSelected(templates.length - 1)
-                                                    return
-                                                }
-                                                setTemplateSelected(templateSelected - 1)
-                                            }}
-                                            className="absolute left-2">
-                                            <IconChevronLeft/>
-                                        </button>
-                                        <button type="button"
-                                            onClick={() => {
-                                                if (templateSelected === templates.length - 1) {
-                                                    setTemplateSelected(0)
-                                                    return
-                                                }
-                                                setTemplateSelected(templateSelected + 1)
-                                            }}
-                                            className="absolute right-2">
-                                            <IconChevronRight/>
-                                        </button>
+                                    <div>
+                                        type
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-span-2 flex items-center justify-center py-3 mt-5">
-                            <button type="submit" className="w-28 rounded-md py-3 border-2 border-white font-bold">
-                                { project ? 'Update' : 'Create' }
-                            </button>
+                            <div className="col-span-1 w-full">
+                                <div className="h-80 md:h-full">
+                                    <div className="h-1/6 flex items-end py-1 justify-center border-b-2 font-bold text-lg">
+                                        <span>Template </span><span className="text-cyan-500 px-1 text-sm">comming soon!!</span>
+                                    </div>
+                                    <div className="h-5/6">
+                                        <div className="h-5/6 flex items-center justify-center">
+                                            { templates[templateSelected].name }
+                                        </div>
+                                        <div className="relative h-1/6 text-white flex items-center">
+                                            <button type="button"
+                                                    onClick={() => {
+                                                        if (templateSelected === 0) {
+                                                            setTemplateSelected(templates.length - 1)
+                                                            return
+                                                        }
+                                                        setTemplateSelected(templateSelected - 1)
+                                                    }}
+                                                    className="absolute left-2">
+                                                <IconChevronLeft/>
+                                            </button>
+                                            <button type="button"
+                                                    onClick={() => {
+                                                        if (templateSelected === templates.length - 1) {
+                                                            setTemplateSelected(0)
+                                                            return
+                                                        }
+                                                        setTemplateSelected(templateSelected + 1)
+                                                    }}
+                                                    className="absolute right-2">
+                                                <IconChevronRight/>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-span-2 w-full flex items-center justify-center py-3 mt-5">
+                                <button type="submit" className="w-28 rounded-md py-3 border-2 border-white font-bold">
+                                    { project ? 'Update' : 'Create' }
+                                </button>
+                            </div>
                         </div>
                     </form>
                 </div>
