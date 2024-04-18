937876f4021165f06827dcf1839b70a126345695
Felipe Sarmiento | FullStack Developer <f.sarmiento.profesional@gmail.com>
2024-04-17 21:16:18 -0500

    Trying to fix viewport settings bug, second solution implemented

--- src/app/MyTemplates/Dashboard/components/subComponents/Options.jsx
+++ src/app/MyTemplates/Dashboard/components/subComponents/Options.jsx
@@ -219,7 +219,7 @@ export const Options = ({options, modifyItemsDashboard, viewport, keepOptions, p
             setZIndex(options['settings' + viewport.type]?.zIndex?.match(/\d+/g) ? options['settings' + viewport.type].zIndex?.match(/\d+/g).map(Number)[0] : "")
 
         }
-    }, [options]);
+    }, [options, viewport, keepOptions])
 
     const [optionItem, setOptionItem] = useState(options);
     const [colorOptions, setColorOptions] = useState({})
